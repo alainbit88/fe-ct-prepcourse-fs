cc0698e911d1044eb0f76cbc24424f36e2189b45
alainbit88 <116106609+alainbit88@users.noreply.github.com>
2025-05-12 19:03:47 -0400

    bucles ejercicio 1

--- C6 Bucles/ejercicios/01.js
+++ C6 Bucles/ejercicios/01.js
@@ -3,6 +3,11 @@ function esIgualYNegativo(a, b) {
   // Determina si son iguales y son ambos negativos.
   // De ser así, retorna true, de lo contrario, retorna false.
   // Tu código:
+  if (a === b && a < 0) console.log(true);
+    else console.log(false);
 }
+
+esIgualYNegativo(-1, -1);
+esIgualYNegativo(-1, -3);
 
 module.exports = esIgualYNegativo;
