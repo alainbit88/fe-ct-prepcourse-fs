6127f37708d38a91b0c6bb356463546337063cd0
alainbit88 <116106609+alainbit88@users.noreply.github.com>
2025-05-25 14:07:11 -0400

    bucles 2 al 5

--- C6 Bucles/ejercicios/03.js
+++ C6 Bucles/ejercicios/03.js
@@ -3,6 +3,12 @@ function obtenerMayor(x, y) {
   // Retorna el número más grande.
   // Si son iguales, retornar cualquiera de los dos.
   // Tu código:
+  if (x > y && x !== y) console.log(x);
+  else console.log(y);
 }
+
+obtenerMayor(2,4);
+obtenerMayor(6,4);
+obtenerMayor(4,4);
 
 module.exports = obtenerMayor;
--- C6 Bucles/ejercicios/05.js
+++ C6 Bucles/ejercicios/05.js
@@ -2,6 +2,10 @@ function esVerdadero(valor) {
   // Si "valor" es verdadero, retorna "Soy verdadero".
   // Caso contrario, retorna "Soy falso".
   // Tu código:
+  if (valor) console.log('Soy verdadero');
+  else console.log('Soy falso');
 }
 
+esVerdadero(true);
+esVerdadero(false);
 module.exports = esVerdadero;
